1f271ee8c999e42b901dc46cb1cbd6f247825f5e
Hussein Aisak <85851097+Huss3n@users.noreply.github.com>
2024-08-20 12:02:53 +0300

    Update 0-subs.py

--- 0x16-api_advanced /0-subs.py
+++ 0x16-api_advanced /0-subs.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 """
-import module
+Script that queries subscribers on a given Reddit subreddit.
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that returns the total number of
-    subscribers for a given subreddit.
-    """
-    base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
-                                                       subreddit=subreddit)
-
-    user_agent = {'User-Agent': 'Python/requests'}
-
-    response = requests.get(api_uri, headers=user_agent,
-                            allow_redirects=False)
-
-    if response.status_code in [302, 404]:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-
-    return response.json().get('data').get('subscribers')
